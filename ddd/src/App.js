f68032a19ec12ea53a00cc0bb167a9dd5342fddd
serhii <peksymov@gmail.com>
2021-07-07 12:56:31 +0300

    hhgff

---
+++ ddd/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, nulla!</p>
+          <p>Dicta est facere, laudantium obcaecati reiciendis ullam veritatis? Eos, in?</p>
+          <p>Accusantium aperiam enim eum impedit, incidunt sunt totam vel voluptate.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
